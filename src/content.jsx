9306ae4c010f8795899a8b297347e155884a98f2
zack wilder <zack.wilder@coinbase.com>
2023-02-22 19:28:43 -0500

    content

--- src/content.jsx
+++ src/content.jsx
@@ -125,7 +125,7 @@ export const timelineText = [
 
 export const rareText = [
 	"A blob is silly, but a BLOB (binary large object) is powerful.",
-	"By combining the two into an NFT, we create a symbol of something that is fun and friendly yet innovative and game-changing; something that from a distance--as a blob--looks like a useless bunch of wasted space, but which up close--as a BLOB--is the data structure helping Ethereum scale to the next level and beyond.",
+	"By combining the two into an NFT, we create a symbol of something that is fun and friendly yet innovative and game-changing; something that from a distance—as a blob—looks like a useless bunch of wasted space, but which up close—as a BLOB—is the data structure helping Ethereum scale to the next level and beyond.",
 ];
 // export const rareText = [
 // 	"When you mint a BlobStar, you mint a vote for humanity. You proudly state that life is not a zero-sum game. It's a game of big and little triumphs, big and little acts of kindness, and it is only won when everyone wins.",
@@ -139,8 +139,8 @@ export const rareText = [
 // export const whyText = [
 // 	"When you mint a BlobStar you choose to live an epic life of humble grandeur.",
 // 	"BlobStars are famous but treat their neighborhood barista really nicely. They're start-up founders and philosophers and quiet revolutionaries but help clean up their local park even when no one is looking. They're genuine, sincere people who do the right thing and know how to have a blast doing it.",
-// 	"A BlobStar is everything. Because they're blobs--so they chill, but they're cartoons, so they're always there for you.",
-// 	"When you mint a BlobStar, you mint a vote for humanity. You mint a vote for what's good and right in the world. You put yourself on the winning team and become a force of good in the world. You say that life is not a zero sum game--it's a game of big success and big kindness and it's only won when everyone wins.",
+// 	"A BlobStar is everything. Because they're blobs—so they chill, but they're cartoons, so they're always there for you.",
+// 	"When you mint a BlobStar, you mint a vote for humanity. You mint a vote for what's good and right in the world. You put yourself on the winning team and become a force of good in the world. You say that life is not a zero sum game—it's a game of big success and big kindness and it's only won when everyone wins.",
 // ];
 
 // export const rareText = [
@@ -154,7 +154,7 @@ export const rareText = [
 // ];
 export const whyText = [
 	"Images, audio, video, and binary executable code, BLOBs effortlessly blend creativity and utility, form and function, beauty and substance.",
-	"BlobStars restore our confidence. They encourage us on our mission to bring blockchain to the world.",
+	"BlobStars restore our confidence. They keep us focused. They encourage us on our mission to bring blockchain to the world.",
 	"And at the very least, those cute little buggers sure do make us smile.",
 ];
 // export const whyText = [
@@ -168,37 +168,37 @@ export const whyText = [
 // export const whyText = [
 // 	"When you mint a BlobStar, you join a community of people who are awesome, fun and friendly.",
 // 	"BlobStars are famous but treat their neighborhood barista really nicely. They're start-up founders and philosophers and revolutionaries but help clean up their local park even when no one is looking.",
-// 	"A BlobStar is everything. Because they're blobs--so they chill, but they're cartoons, so they're always there for you.",
+// 	"A BlobStar is everything. Because they're blobs—so they chill, but they're cartoons, so they're always there for you.",
 // 	"Mint one now and join the band.",
 // ];
 // export const whyText = [
 // 	"When you mint a BlobStar, you join a community of people who are awesome, fun and friendly. You choose to live an epic life of humble grandeur.",
 // 	"BlobStars are famous but treat their neighborhood barista really nicely. They're start-up founders and philosophers and quiet revolutionaries but help clean up their local park even when no one is looking. They have a blast just being kind, good people.",
-// 	"A BlobStar is everything. Because they're blobs--so they chill, but they're cartoons, so they're always there for you.",
+// 	"A BlobStar is everything. Because they're blobs—so they chill, but they're cartoons, so they're always there for you.",
 // 	"Mint one now and join the band.",
 // ];
 // export const whyText = [
 // 	"When you mint a BlobStar, you join a community of people who are awesome, fun and friendly. You choose to live an epic life of humble grandeur.",
 // 	"BlobStars are famous but treat their neighborhood barista really nicely. They're start-up founders and philosophers and quiet revolutionaries but help clean up their local park even when no one is looking. They have a blast just being kind, good people.",
-// 	"A BlobStar is everything. Because they're blobs--so they chill, but they're cartoons, so they're always there for you.",
-// 	"When you mint a BlobStar, you mint a vote for humanity. You say that life is not a zero sum game--it's a game of big success and big kindness and it's only won when everyone wins.",
+// 	"A BlobStar is everything. Because they're blobs—so they chill, but they're cartoons, so they're always there for you.",
+// 	"When you mint a BlobStar, you mint a vote for humanity. You say that life is not a zero sum game—it's a game of big success and big kindness and it's only won when everyone wins.",
 // 	"Mint a BlobStar now and join the cause.",
 // ];
 // export const whyText = [
 // 	"BlobStarsNFT is a community of people who are awesome, fun and friendly. When you mint a BlobStar you choose to live an epic life of humble grandeur.",
 // 	"BlobStars are famous but treat their neighborhood barista really nicely. They're start-up founders and philosophers and quiet revolutionaries but help clean up their local park even when no one is looking. They're genuine, sincere people who do the right thing and know how to have a blast doing it.",
-// 	"A BlobStar is everything. Because they're blobs--so they chill, but they're cartoons, so they're always there for you.",
-// 	"When you mint a BlobStar, you mint a vote for humanity. You mint a vote for what's good and right in the world. You put yourself on the winning team and become a force of good in the world. You say that life is not a zero sum game--it's a game of big success and big kindness and it's only won when everyone wins.",
+// 	"A BlobStar is everything. Because they're blobs—so they chill, but they're cartoons, so they're always there for you.",
+// 	"When you mint a BlobStar, you mint a vote for humanity. You mint a vote for what's good and right in the world. You put yourself on the winning team and become a force of good in the world. You say that life is not a zero sum game—it's a game of big success and big kindness and it's only won when everyone wins.",
 // 	"Mint a BlobStar now and join the cause.",
 // ];
 // export const whyText = [
 // 	"Gain access to a community of people who are epic, fun and friendly. Life is not a choice between being rich, successful, and famous but not a nice person doing not nice things, or being good and kind and giving, but feeling unimportant.",
-// 	"Life is about being famous, but treating the local barista really nicely. About being a revolutionary, but volunteering at the food bank with a smile. About being a paradigm-altering philosopher, but taking the time to tutor underprivileged kids. Or just about having integrity, being a kind, sincere, genuine person who does the right thing--but knows how to have a great time.",
-// 	"A BlobStar is both and everything. Because they're blobs--so they chill, but they're cartoons, so they're always there for you.",
+// 	"Life is about being famous, but treating the local barista really nicely. About being a revolutionary, but volunteering at the food bank with a smile. About being a paradigm-altering philosopher, but taking the time to tutor underprivileged kids. Or just about having integrity, being a kind, sincere, genuine person who does the right thing—but knows how to have a great time.",
+// 	"A BlobStar is both and everything. Because they're blobs—so they chill, but they're cartoons, so they're always there for you.",
 // 	"When you mint a BlobStar, you mint a vote for humanity. You mint a vote for what's good and right in the world. You put yourself on the winning team and become a force of good in the world.",
 // 	"Get the picture? Mint a BlobStar now and join the movement.",
 // ];
 
-// Life isn't a choice between being rich and powerful but not a nice person doing not nice things, or being kind and giving but feeling unimportant. Our Response? A fun, friendly community of good people living epic lives--even if epic just meant treating the local barista nicely.
+// Life isn't a choice between being rich and powerful but not a nice person doing not nice things, or being kind and giving but feeling unimportant. Our Response? A fun, friendly community of good people living epic lives—even if epic just meant treating the local barista nicely.
 
 // Life felt like a choice between being rich and powerful but not a nice person doing not nice things, or being kind and giving but feeling unimportant. Our response? A fun, friendly community of good people living epic lives, even if epic just meant treating the local barista well
